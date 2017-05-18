616b3b871e684b4477bfc1baf551cefcd2e292fc
abraunfels <anastasia.braunfels@gmail.com>
2017-05-19 00:06:36 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -69,13 +69,11 @@ SCENARIO("opertor /=")
 	REQUIRE(b.get_re() == 0.4); 
 	REQUIRE(b.get_im() == 0.2);
 }
-SCENARIO("opertor +")
+SCENARIO("operator +")
 {
 	complex a (1,2);
 	complex b (3,4);
 	double c = 100;
-	a/=b;
-	b/=c;
 	REQUIRE((a+b).get_re() == 4); 
 	REQUIRE((a+b).get_im() == 6);
 	REQUIRE((a+c).get_re() == 101); 
@@ -83,7 +81,7 @@ SCENARIO("opertor +")
 	REQUIRE((c+a).get_re() == 101); 
 	REQUIRE((c+a).get_im() == 2);
 }
-SCENARIO("opertor -")
+SCENARIO("operator -")
 {
 	complex a (10,20);
 	complex b (3,4);
@@ -95,7 +93,7 @@ SCENARIO("opertor -")
 	REQUIRE((c-a).get_re() == -9); 
 	REQUIRE((c-a).get_im() == 20);
 }
-SCENARIO("opertor *")
+SCENARIO("operator *")
 {
 	complex a (1,2);
 	complex b (3,4);
@@ -107,7 +105,7 @@ SCENARIO("opertor *")
 	REQUIRE((c*a).get_re() == 10); 
 	REQUIRE((c*a).get_im() == 20);
 }
-SCENARIO("opertor /")
+SCENARIO("operator /")
 {
 	complex a (1,2);
 	complex b (3,4);
