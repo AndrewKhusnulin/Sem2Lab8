3d2d89920986870ec1e4dd01e87ff5f2539b7f0a
abraunfels <anastasia.braunfels@gmail.com>
2017-05-18 23:45:12 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -29,7 +29,7 @@ SCENARIO("opertor +=")
 	a+=b;
 	b+=c;
 	REQUIRE(a.get_re() == 11); 
-	REQUIRE(a.get_im() == 8);
+	REQUIRE(a.get_im() == 6);
 	REQUIRE(b.get_re() == 106); 
 	REQUIRE(b.get_im() == 4);
 }
@@ -43,7 +43,7 @@ SCENARIO("opertor -=")
 	REQUIRE(a.get_re() == -1); 
 	REQUIRE(a.get_im() == -2);
 	REQUIRE(b.get_re() == 5); 
-	REQUIRE(b.get_im() == 3);
+	REQUIRE(b.get_im() == 4);
 }
 SCENARIO("opertor *=")
 {
@@ -53,7 +53,7 @@ SCENARIO("opertor *=")
 	a*=b;
 	b*=c;
 	REQUIRE(a.get_re() == -5); 
-	REQUIRE(a.get_im() == -10);
+	REQUIRE(a.get_im() == 10);
 	REQUIRE(b.get_re() == 30); 
 	REQUIRE(b.get_im() == 40);
 }
@@ -69,4 +69,45 @@ SCENARIO("opertor /=")
 	REQUIRE(b.get_re() == 0.4); 
 	REQUIRE(b.get_im() == 0.2);
 }
-
+SCENARIO("opertor +")
+{
+	complex a (1,2);
+	complex b (3,4);
+	double c = 100;
+	a/=b;
+	b/=c;
+	REQUIRE((a+b).get_re() == 4); 
+	REQUIRE((a+b).get_im() == 6);
+	REQUIRE((a+c).get_re() == 101); 
+	REQUIRE((a+c).get_im() == 102);
+	REQUIRE((c+a).get_re() == 102); 
+	REQUIRE((c+a).get_im() == 102);
+}
+SCENARIO("opertor -")
+{
+	complex a (10,20);
+	complex b (3,4);
+	double c = 1;
+	a/=b;
+	b/=c;
+	REQUIRE((a-b).get_re() == 17); 
+	REQUIRE((a-b).get_im() == 16);
+	REQUIRE((a-c).get_re() == 9); 
+	REQUIRE((a-c).get_im() == 20);
+	REQUIRE((c-a).get_re() == -9); 
+	REQUIRE((c-a).get_im() == 20);
+}
+SCENARIO("opertor *")
+{
+	complex a (1,2);
+	complex b (3,4);
+	double c = 100;
+	a/=b;
+	b/=c;
+	REQUIRE((a*b).get_re() == -5); 
+	REQUIRE((a*b).get_im() == 10);
+	REQUIRE((a*c).get_re() == 10); 
+	REQUIRE((a*c).get_im() == 20);
+	REQUIRE((c*a).get_re() == 10); 
+	REQUIRE((c*a).get_im() == 20);
+}
